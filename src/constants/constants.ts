f2b92392fae840d1d0097cc05d30fbf0117cac7f
Minahil Ismail <minahilismail44@gmail.com>
2024-08-04 14:14:00 +0500

    fixed env variables

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,7 @@
 import { FaRegClock } from "react-icons/fa6";
 import { IoStatsChartOutline } from "react-icons/io5";
 import { IoSettingsOutline } from "react-icons/io5";
-export const DOMAIN = "http://localhost:3000/api/";
+export const DOMAIN = process.env.NEXTAUTH_URL;
 
 export const FREE_URL_LIMIT = 5;
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -17,7 +17,7 @@ export async function middleware(request: NextRequest) {
     path === "/profile/edit" ||
     path === "/url/add";
 
-  const cookie = request.cookies.get('__Secure-next-auth.session-token'); 
+  const cookie = request.cookies.get('next-auth.session-token'); 
 
   if (isPublicPath && cookie) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
